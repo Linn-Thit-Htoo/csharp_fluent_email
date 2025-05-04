cfd521e4ebb657cc48692df2341bd4df20baa6c0
Lin Thit Htoo <lth1212001@gmail.com>
2025-05-04 15:55:00 +0630

    -

--- csharp_fluent_email/Models/FileModel.cs
+++ csharp_fluent_email/Models/FileModel.cs
@@ -1,9 +1,8 @@
-﻿namespace csharp_fluent_email.Models
+﻿namespace csharp_fluent_email.Models;
+
+public class FileModel
 {
-    public class FileModel
-    {
-        public string FileName { get; set; }
-        public string FilePath { get; set; }
-        public string ContentType { get; set; }
-    }
+    public string FileName { get; set; }
+    public string FilePath { get; set; }
+    public string ContentType { get; set; }
 }
