45d05a958788acc8c3c0a9964929e96daa2b71fb
Lin Thit Htoo <lth1212001@gmail.com>
2025-05-04 15:54:28 +0630

    -

--- csharp_fluent_email/Endpoints/EmailEndpoints.cs
+++ csharp_fluent_email/Endpoints/EmailEndpoints.cs
@@ -2,17 +2,16 @@
 using csharp_fluent_email.Services;
 using Microsoft.AspNetCore.Mvc;
 
-namespace csharp_fluent_email.Endpoints
+namespace csharp_fluent_email.Endpoints;
+
+public static class EmailEndpoints
 {
-    public static class EmailEndpoints
+    public static void UseEmailEndpoints(this IEndpointRouteBuilder app)
     {
-        public static void UseEmailEndpoints(this IEndpointRouteBuilder app)
+        app.MapPost("/api/v1/SendEmail", async ([FromBody] EmailRequestModel requestModel, IEmailService emailService) =>
         {
-            app.MapPost("/api/v1/SendEmail", async ([FromBody] EmailRequestModel requestModel, IEmailService emailService) =>
-            {
-                await emailService.SendEmailAsync(requestModel);
-                return Results.Ok("Email Sending Successful.");
-            }).WithName("SendEmail").WithOpenApi();
-        }
+            await emailService.SendEmailAsync(requestModel);
+            return Results.Ok("Email Sending Successful.");
+        }).WithName("SendEmail").WithOpenApi();
     }
 }
