c1a57fb4ea4635cbca6b4156238d815a83eef59f
Lin Thit Htoo <lth1212001@gmail.com>
2025-05-04 15:54:37 +0630

    -

--- csharp_fluent_email/Extensions/DependencyInjectionExtensions.cs
+++ csharp_fluent_email/Extensions/DependencyInjectionExtensions.cs
@@ -2,38 +2,37 @@
 using System.Net;
 using System.Net.Mail;
 
-namespace csharp_fluent_email.Extensions
+namespace csharp_fluent_email.Extensions;
+
+public static class DependencyInjectionExtensions
 {
-    public static class DependencyInjectionExtensions
+    public static IServiceCollection AddDependencies(this IServiceCollection services, WebApplicationBuilder builder)
     {
-        public static IServiceCollection AddDependencies(this IServiceCollection services, WebApplicationBuilder builder)
+        builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+            .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables();
+
+        builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(opt =>
         {
-            builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
-                .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
-                .AddEnvironmentVariables();
+            opt.SerializerOptions.PropertyNamingPolicy = null;
+        });
 
-            builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(opt =>
-            {
-                opt.SerializerOptions.PropertyNamingPolicy = null;
-            });
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddScoped<IEmailService, EmailService>();
 
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            builder.Services.AddScoped<IEmailService, EmailService>();
+        var smtpClient = new SmtpClient("smtp.gmail.com", 587)
+        {
+            Credentials = new NetworkCredential("lth1212001@gmail.com", ""),
+            EnableSsl = true,
+            UseDefaultCredentials = false
+        };
 
-            var smtpClient = new SmtpClient("smtp.gmail.com", 587)
-            {
-                Credentials = new NetworkCredential("lth1212001@gmail.com", ""),
-                EnableSsl = true,
-                UseDefaultCredentials = false
-            };
+        services
+            .AddFluentEmail("lth1212001@gmail.com")
+            .AddSmtpSender(smtpClient)
+            .AddRazorRenderer();
 
-            services
-                .AddFluentEmail("lth1212001@gmail.com")
-                .AddSmtpSender(smtpClient)
-                .AddRazorRenderer();
-
-            return services;
-        }
+        return services;
     }
 }
