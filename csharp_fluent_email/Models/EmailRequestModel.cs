ae1bd2fc0904094998ffd0658fe1afcc059ec0a0
Lin Thit Htoo <lth1212001@gmail.com>
2025-05-04 15:54:47 +0630

    -

--- csharp_fluent_email/Models/EmailRequestModel.cs
+++ csharp_fluent_email/Models/EmailRequestModel.cs
@@ -1,12 +1,11 @@
-﻿namespace csharp_fluent_email.Models
+﻿namespace csharp_fluent_email.Models;
+
+public class EmailRequestModel
 {
-    public class EmailRequestModel
-    {
-        public List<string> ToEmails { get; set; }
-        public List<string>? CcEmails { get; set; }
-        public List<string>? BccEmails { get; set; }
-        public string Subject { get; set; }
-        public string? Body { get; set; }
-        public List<FileModel>? Files { get; set; }
-    }
+    public List<string> ToEmails { get; set; }
+    public List<string>? CcEmails { get; set; }
+    public List<string>? BccEmails { get; set; }
+    public string Subject { get; set; }
+    public string? Body { get; set; }
+    public List<FileModel>? Files { get; set; }
 }
